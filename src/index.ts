be6ea7a5a85b9ea7fa4df0eedc18ad293a9f4c80
alexraskin <alexraskin@fastmail.fm>
2023-12-04 23:48:40 -0700

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Hono, Context } from 'hono'
+import { Hono, Context } from "hono";
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 import { R2Object } from "@cloudflare/workers-types";
@@ -15,16 +15,16 @@ app.get("/", async (c: Context) => {
 });
 
 app.get("/cosmo", async (c: Context) => {
-
   const prefix = "cosmo/";
 
   const response = await c.env.CAT_BUCKET.list();
 
   const keys = response.objects.map((obj: { key: R2Object }) => obj.key);
 
-  const keysWithPrefix = keys.filter((key: string)  => key.startsWith(prefix));
+  const keysWithPrefix = keys.filter((key: string) => key.startsWith(prefix));
 
-  const randomKey = keysWithPrefix[Math.floor(Math.random() * keysWithPrefix.length)];
+  const randomKey =
+    keysWithPrefix[Math.floor(Math.random() * keysWithPrefix.length)];
 
   const photo_data: PhotoResponseData = {
     photoUrl: `https://cdn.cat.network/${randomKey}`,
@@ -35,23 +35,23 @@ app.get("/cosmo", async (c: Context) => {
 });
 
 app.get("/bczs", async (c: Context) => {
-  
-    const prefix = "bczs/";
-  
-    const response = await c.env.CAT_BUCKET.list();
-  
-    const keys = response.objects.map((obj: { key: R2Object }) => obj.key);
-  
-    const keysWithPrefix = keys.filter((key: string)  => key.startsWith(prefix));
-  
-    const randomKey = keysWithPrefix[Math.floor(Math.random() * keysWithPrefix.length)];
-  
-    const photo_data: PhotoResponseData = {
-      photoUrl: `https://cdn.cat.network/${randomKey}`,
-      statusCode: 200,
-    };
-  
-    return c.json(photo_data, 200);
+  const prefix = "bczs/";
+
+  const response = await c.env.CAT_BUCKET.list();
+
+  const keys = response.objects.map((obj: { key: R2Object }) => obj.key);
+
+  const keysWithPrefix = keys.filter((key: string) => key.startsWith(prefix));
+
+  const randomKey =
+    keysWithPrefix[Math.floor(Math.random() * keysWithPrefix.length)];
+
+  const photo_data: PhotoResponseData = {
+    photoUrl: `https://cdn.cat.network/${randomKey}`,
+    statusCode: 200,
+  };
+
+  return c.json(photo_data, 200);
 });
 
 export default app;
